117a6e1d84588a68e6e6747f6170f0879d1ec1a8
adikotak <adikotak@gmail.com>
2017-11-24 03:06:15 -0800

    final project done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
+
 $(document).ready(function() {
+  //JQuery Function 1
   $("#down_button").hover(function() {
+    //JQuery Function 2
     $(this).css("cursor", "pointer");
   })
 })
 
 $(document).ready(function() {
+  //JQuery Function 3
   $("#down_button").click(function() {
     $("html, body").animate({ scrollTop: $(document).height() }, "slow");
   })
@@ -12,9 +16,12 @@ $(document).ready(function() {
 
 $(document).ready(function(){
   $(window).scroll(function(){
+      //JQuery Function 4
       if ($(this).scrollTop() > 200) {
+          //JQuery Function 5
           $('#navbar').fadeIn(500);
       } else {
+          //JQuery Function 6
           $('#navbar').fadeOut(500);
       }
   })
